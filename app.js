de2377a83ba5417379ce527eaedfffdec160d9c0
Naman Goyal <namangoyal4900@gmail.com>
2020-09-25 22:20:36 +0530

    last heroku deploy test

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use("/project-astitva", astitva);
 const homePage = require("./Home");
 app.use("/", homePage);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`server is now running on port ${PORT}`);
